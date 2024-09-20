bd9bc55a4afd1f4e976abfccc45b76716249d4d7
matheus rocha dos santos <s.rocha17@alunoescolassociais.org.br>
2024-09-20 11:38:31 -0300

    função para prencher os campos logradora,endeço,bairro e uf

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';  //Ativa o modo restrito //Este modo faz com que o javascripti opere de formas mais segura e rigorosa, ajudando a evitar erros comuns  de programação 
 /* consumo de API - https://viacep.com.br/ */ 
 
-//função para ;limpar campos preenchidos
+//função para limpar campos preenchidos
 
 const limparFormulario = () => {
     document.getElementById('logradoro').valeu = '';
@@ -16,3 +16,12 @@ const limparFormulario = () => {
 //Verifica se o cep é válido 
 const eNumero = (numero) => /^[0-9]+$/.test(numero);// testa a espreção no argumento
 const cepValido =(cep) => cep.length == 8 && eNumero(cep)// indentifica o tanto de numero indentificado no cep 
+
+//função preenchrer formulário
+
+const preencherformulario = (endereco) =>{
+    document.getElementById('logradoro').value = endereco.logradoro;
+    document.getElementById('localidade').value = endereco.localidade;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('uf').value = endereco.uf;
+}
