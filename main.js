a7098dfc4e546435c0a5ea5622d50a6e9db56768
matheus rocha dos santos <s.rocha17@alunoescolassociais.org.br>
2024-10-02 11:03:25 -0300

    ajuste

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
 'use strict'; //Modo "Restrito"
 //Consumindo API de CEP, do ViaCep
 // https://viacep.com.br/
- 
+
 //Limpa o Form (do CEP pra baixo)...
-const limparFormulario = () =>{
+const limparFormulario = () => {
     document.getElementById('logradouro').value = '';
     document.getElementById('bairro').value = '';
     document.getElementById('localidade').value = '';
     document.getElementById('uf').value = '';
 }
- 
+
 //Preenche os campos relacionados ao CEP...
-const preencherForumulario = (endereco) =>{
+const preencherForumulario = (endereco) => {
     document.getElementById('logradouro').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
     document.getElementById('localidade').value = endereco.localidade;
     document.getElementById('uf').value = endereco.uf;
 }
- 
+
 //Verifica se o CEP é válido...
 const eNumero = (numero) => /^[0-9]+$/.test(numero); //Expressão Regular
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
- 
+
 //Consumindo API... 2- passo
-const pesquisarCep = async() => {
+const pesquisarCep = async () => {
     limparFormulario();
     const url = `http://viacep.com.br/ws/${cep.value}/json/`;
-    if(cepValido(cep.value)){
+    if (cepValido(cep.value)) {
         const dados = await fetch(url); //await = esperar
         const addres = await dados.json(); // fetch = promessa
-       
+
         // hasOwnProperty  retorna um booleano indicando se o objeto possui a propriedade 
-        //especificada como uma propriedade definida no próprio objeto em questão.
-        if(addres.hasOwnProperty('erro')){
+        //especificada como uma propriedade definida no próprio objeto em questão
+        if (addres.hasOwnProperty('erro')) {
             // document.getElementById('rua').value = 'CEP não encontrado!';
             alert('CEP não encontrado!');
-        }else {
+        } else {
             preencherForumulario(addres);
         }
-    }else{
+    } else {
         // document.getElementById('rua').value = 'CEP incorreto!';
         alert('CEP incorreto!');
     }
 }
- 
+
 //Adicionando um evento DOM, no input CEP... 1- passo
 document.getElementById('cep').addEventListener('focusout', pesquisarCep);
